bee8569589aeff5e319b61ef5181bee9b4b9bb8c
Merrick Liu <merrickliu168@gmail.com>
2025-01-20 17:58:49 -0500

    Ovulation prediction dummy function

---
+++ app/src/main/java/com/tpp/theperiodpurse/utility/OvulationPrediction.kt
@@ -0,0 +1,7 @@
+package com.tpp.theperiodpurse.utility
+
+import com.tpp.theperiodpurse.data.entity.Date as TPPDate
+
+fun calculateAverageOvulationLength(periodHistory: ArrayList<TPPDate>): Float {
+    return -1f
+}
